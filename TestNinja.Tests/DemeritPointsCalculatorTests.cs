c7c18d79505497baed235ccf3e878404d5657a8a
le6o <sktomov@gmail.com>
2019-02-04 22:10:26 +0200

    adding more tests

---
+++ TestNinja.Tests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private  DemeritPointsCalculator _demeritPointsCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _demeritPointsCalculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(1000)]
+        public void CalculateDemeritPoints_InvalidInput_ThrowsAnException(int speed)
+        {
+            Assert.That(() => _demeritPointsCalculator.CalculateDemeritPoints(speed), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(10)]
+        [TestCase(65)]
+        public void CalculateDemeritPoints_InputInLimit_ReturnsZero(int speed)
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        public void CalculateDemeritPoints_InputAboveLimit_ReturnsDemeritPoints()
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(99);
+
+            Assert.That(result, Is.EqualTo(4));
+        }
+    }
+}
---
+++ TestNinja.Tests/StackTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class StackTests
+    {
+
+        [Test]
+        public void Stack_NewStack_ReturnsCountOfZero()
+        {
+            var stack = new Stack<int>();
+
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Push_NullObject_ThrowsAnException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ValidArgs_AddTheObjectToStack()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(1);
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowsAnException()
+        {
+            var stack = new Stack<int>();
+
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_ValidStack_PopLastEnteredObject()
+        {
+            var stack = new Stack<int>();
+            stack.Push(-1);
+
+            var result = stack.Pop();
+
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Pop_ValidStack_AfterPopCountIsSmaller()
+        {
+            var stack = new Stack<int>();
+            stack.Push(-1);
+            stack.Push(-2);
+
+            var result = stack.Pop();
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowsAnException()
+        {
+            var stack = new Stack<bool>();
+
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_ValidStack_ReturnsLastObject()
+        {
+            var stack = new Stack<string>();
+
+            stack.Push("ala");
+            stack.Push("bala");
+
+            var result = stack.Peek();
+
+            Assert.That(result, Is.EqualTo("bala"));
+        }
+
+        [Test]
+        public void Peek_ValidStack_CountOfStackIsSame()
+        {
+            var stack = new Stack<string>();
+
+            stack.Push("ala");
+            stack.Push("bala");
+
+            var result = stack.Peek();
+
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+
+    }
+}
