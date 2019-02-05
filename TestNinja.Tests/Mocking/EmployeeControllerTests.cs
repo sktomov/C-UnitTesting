cb91428ab64e9201d0f6affaa58c9e2dd631a221
le6o <sktomov@gmail.com>
2019-02-05 22:27:22 +0200

    testing employee controller

---
+++ TestNinja.Tests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace UnitTestProject1.Mocking
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteEmployee()
+        {
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            controller.DeleteEmployee(1);
+
+            storage.Verify(x => x.DeleteEmployee(1));
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_ReturnsRetirectResult()
+        {
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            var result = controller.DeleteEmployee(1);
+
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+        }
+    }
+}
